ff2fcc5ec72182de9c5d989ce67ef3e8f39d1983
Tatiya Seehatrakul <swbt.tatiya@gmail.comgit config --global user.name Tatiya>
2024-10-13 03:20:54 +0700

    feat: create _navbar_welcome_page.html.erb and render in application
    - create _navbar_welcome_page.html.erb
    - render _navbar_welcome_page.html.erb in application.html.erb and application_controller.rb
    - Add dept to buttons in welcome_page and _navbar_welcome_page.html.erb

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  helper_method :show_navbar?
+
+  def show_navbar?
+    # Logic to determine if the navbar should be shown
+    # Example: Show navbar except on the login or signup pages
+    !(controller_name == 'welcome_page' && action_name == 'index') 
+    # &&
+    # !(controller_name == 'registrations' && action_name == 'new')
+  end
 
   def after_sign_in_path_for(resource)
     root_path  # Redirect to the homepage after login

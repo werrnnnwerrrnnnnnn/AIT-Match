62608c031862d19ca5914c30844446e9cd4baa65
Tatiya Seehatrakul <swbt.tatiya@gmail.comgit config --global user.name Tatiya>
2024-10-13 19:45:12 +0700

    fix: enable Devise validation and minimum password length

--- app/app/models/user.rb
+++ app/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable #, :validatable
+         :recoverable, :rememberable, :validatable
 
   after_initialize :set_default_role, if: :new_record?
 
@@ -11,12 +11,8 @@ class User < ApplicationRecord
   end
 
   #Validates for Sign up form
-  validates :email, presence: { message: "Email can't be blank" },
-                    uniqueness: { message: "has already been taken" },
-                    format: { 
-                      with: /\Ast\d{6}@(ait\.ac\.th|ait\.asia)\z/, 
-                      message: "must follow AIT format"
+  validates :email, format: { 
+                    with: /\Ast\d{6}@(ait\.ac\.th|ait\.asia)\z/, 
+                    message: "must follow AIT format"
                     }
-
-  validates :password, presence: { message: "can't be blank" }
 end
